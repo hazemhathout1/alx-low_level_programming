98624ea8236a04a63c1deb5c2d45a64e33466d8d
hazemhathout1 <hazemhathout1@gmail.com>
2023-08-10 06:07:38 -0700

    test

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ *main - Entry point Function
+ *
+ *description: Function return nothing
+ *
+ *Return: Integer return type
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
