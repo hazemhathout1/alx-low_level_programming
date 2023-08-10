0590490caffcd1274c36e64e6360b7933ef3d9be
hazemhathout1 <hazemhathout1@gmail.com>
2023-08-10 05:56:22 -0700

    test

---
+++ 4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/*
+ *main : Entry point Function
+ *
+ *Description : Function return nothing
+ *
+ *Return : Integer return type
+ */
+
+
+int main(void)
+{
+char a[] = "Programming is like building a multilingual puzzle";
+puts(a);
+return (0);
+}
