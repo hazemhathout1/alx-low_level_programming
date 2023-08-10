c431457d5857ee5ef14a3b1f009e54ecf1dba66b
hazemhathout1 <hazemhathout1@gmail.com>
2023-08-10 06:12:27 -0700

    test

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ *main - Entry point Function
+ *
+ *description: Function return nothing
+ *
+ *Return: Integer return type
+ */
+int main(void)
+{
+printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of long int: %d byte(s)\n", sizeof(long int));
+printf("Size of long long: %d byte(s)\n", sizeof(long long));
+printf("Size of float: %d byte(s)\n", sizeof(float));
+return (0);
+}
