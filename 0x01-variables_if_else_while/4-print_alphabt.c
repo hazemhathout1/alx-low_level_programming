ad0691f7b567921cab8c48c060350163067ddbda
hazemhathout1 <hazemhathout1@gmail.com>
2023-08-11 10:31:01 -0700

    test

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ *main - Entry point Function
+ *
+ *Description: Function return nothing here
+ *
+ *Return: 0 Always
+ */
+int main(void)
+{
+int x = 97;
+while (x != 123)
+{
+if (x == 101 || x == 113)
+{
+x++;
+continue;
+}
+putchar(x);
+x++;
+}
+}
