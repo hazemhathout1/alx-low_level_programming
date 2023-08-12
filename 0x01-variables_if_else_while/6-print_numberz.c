dc4b4308529d81fb8fe50adeb76ad0ff19a32ed0
hazemhathout1 <hazemhathout1@gmail.com>
2023-08-11 18:24:03 -0700

    test

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ *main - Entry point Function
+ *
+ *Description: Function return nothing here
+ *
+ *Return: 0 Always
+ */
+int main(void)
+{
+int i = 48;
+while (i < 58)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+}
