01302b94e3212b397e05df5c27a8c58b3ebad57c
hazemhathout1 <hazemhathout1@gmail.com>
2023-08-15 05:01:43 -0700

    test

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,15 @@
+/**
+ *_islower - Entry point Function
+ *
+ *Description: Function return nothing here
+ *
+ *Return: 0 Always
+ */
 int _islower(int c)
 {
   if(c>90)
 {
 return (1);
 }
-else return (0)
+else return (0);
 }
