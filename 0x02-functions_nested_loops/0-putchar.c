572ac2f22ed784af4c3f0a1198068f43dc57d490
hazemhathout1 <hazemhathout1@gmail.com>
2023-08-15 04:21:56 -0700

    test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ *main - Entry point Function
+ *
+ *Description: Function return nothing
+ *
+ *Return: Integer return type
+ */
+int main(void)
+{
+char x[] = "Hazem";
+int c = 0;
+while (x[c] != '\0')
+{
+putchar(x[c]);
+c++;
+}
+return (0);
+}
