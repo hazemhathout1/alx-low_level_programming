a4f7768ee8a4ad279b28ea3dd216f4baf4eaa81e
hazemhathout1 <hazemhathout1@gmail.com>
2023-08-15 04:36:54 -0700

    test

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+/**
+ *print_alphabet - Entry point Function
+ *
+ *Description: Function return nothing here
+ *
+ *Return: 0 Always
+ */
+void print_alphabet(void)
+{
+int x = 97;
+while (x != 123)
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
+
+}
